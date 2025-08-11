4ade105f764bf3cde8113267a556dd3ad1f71662
UlinduRasantha <rasanthaulindu8888@gmail.com>
2025-08-11 11:48:26 +0530

    update

--- client/src/pages/MoveDetails.jsx
+++ client/src/pages/MoveDetails.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const MoveDetails = () => {
+  const {id} = useParams()
+  const [show, setShow] = useState(null)
   return (
     <div>MoveDetails</div>
   )
