b351869fd85a42467c62d9d0ab4710fddd86d333
UlinduRasantha <rasanthaulindu8888@gmail.com>
2025-08-15 12:25:50 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,11 @@ import MyBookings from './pages/MyBookings.jsx'
 import Favorite from './pages/Favorite.jsx'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer.jsx'
+import Layout from './pages/admin/Layout.jsx'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import AddShows from './pages/admin/AddShows.jsx'
+import ListShows from './pages/admin/ListShows.jsx'
+import ListBookings from './pages/admin/ListBookings.jsx'
 
 
 const App = () => {
@@ -27,6 +32,12 @@ const App = () => {
             <Route path='/movies/:id/:date' element = { <SeatLayout /> } />
             <Route path='/my-bookings' element = { <MyBookings /> } />
             <Route path='/favorite' element = { <Favorite /> } />
+            <Route path='/admin/*' element={<Layout />}>
+                <Route index element={<Dashboard />} />
+                <Route path='add-shows' element={<AddShows />} />
+                <Route path='list-shows' element={<ListShows />} />
+                <Route path='list-bookings' element={<ListBookings />} />
+            </Route>
         </Routes>
         {!isAdminRoute && <Footer />}
     </>
---
+++ client/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,70 @@
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UserIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
+import BlurCircle from '../../components/BlurCircle';
+
+const Dashboard = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [dashbordData, setDashbordData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0
+  });
+
+  const [loading, setLoading]  = useState(true);
+
+  const dashboardCasrd = [
+    {title: "Total Bookings", value: dashbordData.totalBookings || "0",
+       icon:ChartLineIcon},
+    {title: "Total Revenue", value: currency + dashbordData.totalRevenue || "0",
+       icon:CircleDollarSignIcon},
+    {title: "Active Shows", value: dashbordData.activeShows.length || "0",
+       icon:PlayCircleIcon},
+    {title: "Total Bookings", value: dashbordData.totalUser || "0",
+       icon:UserIcon },
+
+  ]
+
+
+  const fetchDashbordData = async () => {
+    setDashbordData(dummyDashboardData)
+    setLoading(false)
+  };
+
+  useEffect(()=> {
+    fetchDashbordData();
+  },[]);
+
+
+  return  !loading ?(
+    <>
+      <Title text1="Admin" text2="Dashboard"/>
+
+      <div className='relative flex flex-wrap gap-4 mt-6'>
+          <BlurCircle top='-100px' left='0' />
+          <div className='flex flex-wrap gap-4 w-full'>
+            {dashboardCasrd.map((card, index) => (
+              <div key={index} className='flex items-center
+              justify-between px-4 py-3 bg-primary/10 border
+              border-primary/20 rounded-md max-w-50 w-full'>
+                <div>
+                  <h1 className='text-sm'>{card.title}</h1>
+                  <p className='text-xl font-medium mt-1'>{card.value}
+                  </p>
+                </div>
+                <card.icon className = 'w-6 h-6' />
+
+              </div>
+            ))}
+          </div>
+      </div>
+    </>
+  ) : <Loading />
+}
+
+export default Dashboard
